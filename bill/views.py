404073ed5f108250e7c2048ba328a7db24b2c6bb
thewayiam <twly.tw@gmail.com>
2014-01-16 15:22:05 +0800

    bills search include summary field, coating vote pie chart text

--- bill/views.py
+++ bill/views.py
@@ -15,8 +15,8 @@ def bills(request, keyword_url, index):
     elif keyword_url:
         keyword = keyword_url.strip()
     if keyword:
-        bills = Bill.objects.filter(reduce(operator.or_, (Q(abstract__icontains=x) for x in keyword.split())))
-        query = Q(reduce(operator.or_, (Q(abstract__icontains=x) for x in keyword.split())))
+        bills = Bill.objects.filter(reduce(operator.or_, (Q(abstract__icontains=x) | Q(summary__icontains=x) for x in keyword.split())))
+        query = Q(reduce(operator.or_, (Q(abstract__icontains=x) | Q(summary__icontains=x) for x in keyword.split())))
         if bills:
             keyword_obj = Keyword.objects.filter(category=3,content=keyword.strip())
             if keyword_obj:
@@ -25,7 +25,7 @@ def bills(request, keyword_url, index):
                 k = Keyword(content=keyword.strip(),category=3,valid=True,hits=1)
                 k.save()
     if index == 'normal':
-        bills = Bill.objects.filter(query, last_action__isnull=False, abstract__isnull=False).order_by('-last_action_at')[:100]
+        bills = Bill.objects.filter(query, last_action__isnull=False).order_by('-last_action_at')[:100]
     elif index == 'rejected':
         bills = Bill.objects.filter(query & Q(ttsmotions__progress='退回程序')).annotate(totalNum=Count('ttsmotions__id')).filter(totalNum__gt=1).order_by('-totalNum')
     return render(request,'bill/bills.html', {'index':index, 'keyword_obj':keyword_list(3), 'keyword':keyword, 'bills':bills})
