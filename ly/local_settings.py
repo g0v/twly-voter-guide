3cdb151d33e8a0b0dfb968901dc20e0a66a2f250
thewayiam <twly.tw@gmail.com>
2014-02-09 00:58:23 +0800

    adjust settings for travis-ci

--- ly/local_settings.py
+++ ly/local_settings.py
@@ -1,13 +1,3 @@
-"""Local test settings and globals which allows us to run our
-test suite locally."""
-from .settings import *
-
-
-########## TEST SETTINGS
-TEST_RUNNER = 'discover_runner.DiscoverRunner'
-TEST_DISCOVER_TOP_LEVEL = PROJECT_ROOT
-TEST_DISCOVER_ROOT = PROJECT_ROOT
-TEST_DISCOVER_PATTERN = "*"
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -20,4 +10,4 @@ DATABASES = {
             'TEST_CHARSET':'UTF8'
             }
         }
-SECRET_KEY = 'w%3uknhc-5l1nmmh7tp#@29!nthqvc2fb#&!r#x(1(!0ixpwo'
+SECRET_KEY = ''
