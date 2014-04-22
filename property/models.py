aef7589170a3e2ff53a643e23a6e1c577aac5e8c
thewayiam <twly.tw@gmail.com>
2014-04-22 22:17:55 +0800

    #5: personal_property car done

---
+++ property/models.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+
+class Stock(models.Model):
+    source_file = models.TextField()
+    index = models.IntegerField()
+    legislator = models.ForeignKey('legislator.Legislator', to_field="uid")
+    date = models.DateTimeField()
+    category = models.TextField()
+    name = models.TextField()
+    owner = models.TextField(blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    face_value = models.FloatField(blank=True, null=True)
+    currency = models.TextField()
+    total = models.FloatField()
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ('source_file', 'index',)
+
+class Land(models.Model):
+    source_file = models.TextField()
+    index = models.IntegerField()
+    legislator = models.ForeignKey('legislator.Legislator', to_field="uid")
+    date = models.DateTimeField()
+    category = models.TextField()
+    name = models.TextField()
+    area = models.FloatField()
+    share_portion = models.TextField()
+    portion = models.FloatField()
+    owner = models.TextField(blank=True, null=True)
+    register_date = models.TextField(blank=True, null=True)
+    register_reason = models.TextField(blank=True, null=True)
+    acquire_value = models.TextField(blank=True, null=True)
+    total = models.FloatField()
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ('source_file', 'index',)
+
+class Building(models.Model):
+    source_file = models.TextField()
+    index = models.IntegerField()
+    legislator = models.ForeignKey('legislator.Legislator', to_field="uid")
+    date = models.DateTimeField()
+    category = models.TextField()
+    name = models.TextField()
+    area = models.FloatField()
+    share_portion = models.TextField()
+    portion = models.FloatField()
+    owner = models.TextField(blank=True, null=True)
+    register_date = models.TextField(blank=True, null=True)
+    register_reason = models.TextField(blank=True, null=True)
+    acquire_value = models.TextField(blank=True, null=True)
+    total = models.FloatField()
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ('source_file', 'index',)
+
+class Boat(models.Model):
+    source_file = models.TextField()
+    index = models.IntegerField()
+    legislator = models.ForeignKey('legislator.Legislator', to_field="uid")
+    date = models.DateTimeField()
+    category = models.TextField()
+    name = models.TextField()
+    tonnage = models.FloatField()
+    homeport = models.TextField()
+    owner = models.TextField(blank=True, null=True)
+    register_date = models.TextField(blank=True, null=True)
+    register_reason = models.TextField(blank=True, null=True)
+    acquire_value = models.TextField(blank=True, null=True)
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ('source_file', 'index',)
+
+class Car(models.Model):
+    source_file = models.TextField()
+    index = models.IntegerField()
+    legislator = models.ForeignKey('legislator.Legislator', to_field="uid")
+    date = models.DateTimeField()
+    category = models.TextField()
+    name = models.TextField()
+    capacity = models.FloatField()
+    owner = models.TextField(blank=True, null=True)
+    register_date = models.TextField(blank=True, null=True)
+    register_reason = models.TextField(blank=True, null=True)
+    acquire_value = models.TextField(blank=True, null=True)
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ('source_file', 'index',)
