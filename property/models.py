9f37a2b4015dd7261ce83deb26e15716fbee9893
thewayiam <twly.tw@gmail.com>
2014-04-23 23:34:12 +0800

    #15: property aircraft done

--- property/models.py
+++ property/models.py
@@ -9,11 +9,14 @@ class Stock(models.Model):
     date = models.DateTimeField()
     category = models.TextField()
     name = models.TextField()
+    symbol = models.IntegerField(blank=True, null=True)
     owner = models.TextField(blank=True, null=True)
     quantity = models.IntegerField(blank=True, null=True)
     face_value = models.FloatField(blank=True, null=True)
+    market_value = models.FloatField(blank=True, null=True)
     currency = models.TextField()
     total = models.FloatField()
+    total_value = models.FloatField(blank=True, null=True)
     def __unicode__(self):
         return self.name
 
@@ -27,6 +30,7 @@ class Land(models.Model):
     date = models.DateTimeField()
     category = models.TextField()
     name = models.TextField()
+    market_value = models.FloatField(blank=True, null=True)
     area = models.FloatField()
     share_portion = models.TextField()
     portion = models.FloatField()
@@ -35,6 +39,7 @@ class Land(models.Model):
     register_reason = models.TextField(blank=True, null=True)
     acquire_value = models.TextField(blank=True, null=True)
     total = models.FloatField()
+    total_value = models.FloatField(blank=True, null=True)
     def __unicode__(self):
         return self.name
 
@@ -48,6 +53,7 @@ class Building(models.Model):
     date = models.DateTimeField()
     category = models.TextField()
     name = models.TextField()
+    market_value = models.FloatField(blank=True, null=True)
     area = models.FloatField()
     share_portion = models.TextField()
     portion = models.FloatField()
@@ -56,6 +62,7 @@ class Building(models.Model):
     register_reason = models.TextField(blank=True, null=True)
     acquire_value = models.TextField(blank=True, null=True)
     total = models.FloatField()
+    total_value = models.FloatField(blank=True, null=True)
     def __unicode__(self):
         return self.name
 
@@ -89,6 +96,25 @@ class Car(models.Model):
     category = models.TextField()
     name = models.TextField()
     capacity = models.FloatField()
+    owner = models.TextField(blank=True, null=True)
+    register_date = models.TextField(blank=True, null=True)
+    register_reason = models.TextField(blank=True, null=True)
+    acquire_value = models.TextField(blank=True, null=True)
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ('source_file', 'index',)
+
+class Aircraft(models.Model):
+    source_file = models.TextField()
+    index = models.IntegerField()
+    legislator = models.ForeignKey('legislator.Legislator', to_field="uid")
+    date = models.DateTimeField()
+    category = models.TextField()
+    name = models.TextField()
+    maker = models.TextField()
+    number = models.TextField()
     owner = models.TextField(blank=True, null=True)
     register_date = models.TextField(blank=True, null=True)
     register_reason = models.TextField(blank=True, null=True)
