fb51ae52ced632ada430aca4db39fcb4b2e07a56
thewayiam <twly.tw@gmail.com>
2014-05-08 22:54:41 +0800

    votes can select result, keyword colorfy

--- vote/views.py
+++ vote/views.py
@@ -8,10 +8,16 @@ from search.views import keyword_list, keyword_been_searched, keyword_normalize
 
 
 def votes(request, keyword_url, index='normal'):
+    result = None
     if index == 'conscience':
         query = Q(conflict=True)
     else:
         query = Q()
+    #--> 依表決結果分類
+    if 'result' in request.GET:
+        result = request.GET['result']
+        query = query & Q(result=result)
+    #<--
     keyword = keyword_normalize(request, keyword_url)
     if keyword:
         votes = Vote.objects.filter(query & reduce(operator.and_, (Q(content__icontains=x) for x in keyword.split()))).order_by('-sitting__date', 'vote_seq')
@@ -19,7 +25,7 @@ def votes(request, keyword_url, index='normal'):
             keyword_been_searched(keyword, 2)
     else:
         votes = Vote.objects.filter(query).order_by('-sitting__date', 'vote_seq')
-    return render(request,'vote/votes.html', {'votes': votes, 'index':index, 'keyword':keyword, 'keyword_obj':keyword_list(2)})
+    return render(request,'vote/votes.html', {'votes': votes, 'index':index, 'keyword':keyword, 'result':result, 'keyword_obj':keyword_list(2)})
 
 def vote(request, vote_id):
     data = None
