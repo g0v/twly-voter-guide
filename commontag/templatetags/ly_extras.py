9d84d24a11c49852080bd026e66613a9ba4279be
thewayiam <twly.tw@gmail.com>
2015-06-19 07:09:07 +0800

    css adjust

--- commontag/templatetags/ly_extras.py
+++ commontag/templatetags/ly_extras.py
@@ -173,7 +173,7 @@ def subtract(value, arg):
     return value - arg
 
 @register.filter(name='multiply')
-def subtract(value, arg):
+def multiply(value, arg):
     return value * arg
 
 @register.filter(name='divide')
--- ly/views.py
+++ ly/views.py
@@ -20,7 +20,7 @@ def reference(request):
 def home(request):
     results = SearchQuerySet().filter(content=request.GET['keyword']).models(Vote, Bill) if request.GET.get('keyword') else []
     keywords = [x.content for x in SearchQuerySet().models(Keyword).order_by('-hits')]
-    return render(request, 'home.html', {'results': results, 'keyword': request.GET.get('keyword'), 'keyword_obj': keywords, 'hot_keyword': keywords[:6]})
+    return render(request, 'home.html', {'results': results, 'keyword': request.GET.get('keyword', ''), 'keyword_obj': keywords, 'hot_keyword': keywords[:6]})
 
 def logout(request):
     auth.logout(request)
